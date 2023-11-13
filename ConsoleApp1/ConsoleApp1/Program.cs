2ca5f9fed32a351edcd0dcde5f06d37d921cb58d
RohitKokare810 <Rohitkokare810@gmail.com>
2023-11-13 14:55:46 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Initialize jagged array with student scores
+        int[][] studentScores = new int[][]
+        {
+            new int[] {85, 92, 78},
+            new int[] {90, 87, 93, 89},
+            new int[] {76, 88}
+        };
+
+        // Calculate and print average score for all students combined
+        double totalAverage = CalculateAverage(studentScores);
+        Console.WriteLine($"Average score for all students combined: {totalAverage:F2}");
+
+        // Calculate and print average score for each student
+        for (int i = 0; i < studentScores.Length; i++)
+        {
+            double studentAverage = CalculateAverage(studentScores[i]);
+            Console.WriteLine($"Average score for Student {i + 1}: {studentAverage:F2}");
+        }
+
+        // Print scores of each student using nested loops
+        for (int i = 0; i < studentScores.Length; i++)
+        {
+            Console.Write($"Student {i + 1} Scores: ");
+            for (int j = 0; j < studentScores[i].Length; j++)
+            {
+                Console.Write($"{studentScores[i][j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Function to calculate average of an array of integers
+    static double CalculateAverage(int[] scores)
+    {
+        if (scores.Length == 0)
+        {
+            return 0; // Avoid division by zero
+        }
+
+        int sum = 0;
+        foreach (int score in scores)
+        {
+            sum += score;
+        }
+
+        return (double)sum / scores.Length;
+    }
+
+    // Function to calculate average of a jagged array of integers
+    static double CalculateAverage(int[][] scores)
+    {
+        if (scores.Length == 0)
+        {
+            return 0; // Avoid division by zero
+        }
+
+        int totalSum = 0;
+        int totalLength = 0;
+
+        foreach (int[] studentScores in scores)
+        {
+            foreach (int score in studentScores)
+            {
+                totalSum += score;
+                totalLength++;
+            }
+        }
+
+        return (double)totalSum / totalLength;
+        
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class SalaryCalculationSystem
+{
+    static void Main()
+    {
+        Console.WriteLine("Salary Calculation System");
+
+        // Get employee type
+        Console.WriteLine("Choose Employee Type:");
+        Console.WriteLine("1. HR");
+        Console.WriteLine("2. Admin");
+        Console.WriteLine("3. Software Developer");
+
+        int employeeType = int.Parse(Console.ReadLine());
+
+        // Get input based on employee type
+        int workingHours, numberOfWorkingDays, projectHandles, extras;
+        Console.WriteLine("Enter working hours:");
+        workingHours = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter number of working days:");
+        numberOfWorkingDays = int.Parse(Console.ReadLine());
+
+        switch (employeeType)
+        {
+            case 1: // HR
+                projectHandles = 1;
+                extras = 0;
+                break;
+            case 2: // Admin
+                projectHandles = 0; // Admin has no project handles
+                extras = 0;
+                break;
+            case 3: // Software Developer
+                Console.WriteLine("Enter project handles:");
+                projectHandles = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter extras:");
+                extras = int.Parse(Console.ReadLine());
+                break;
+            default:
+                Console.WriteLine("Invalid Employee Type");
+                return;
+        }
+
+        // Calculate and display the monthly salary
+        double monthlySalary = CalculateMonthlySalary(workingHours, numberOfWorkingDays, projectHandles, extras);
+        Console.WriteLine($"Monthly Salary: ${monthlySalary:F2}");
+    }
+
+    // Method to calculate monthly salary based on employee type
+    static double CalculateMonthlySalary(int workingHours, int numberOfWorkingDays, int projectHandles, int extras)
+    {
+        double salary = workingHours * numberOfWorkingDays * 100 + projectHandles * 3000 + extras * 2000;
+        return salary;
+    }
+}
